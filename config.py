16784f418f258ecfad46855afab7093ac65b77b7
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-12-02 10:57:21 +0100

    [feat]: refactored for multiple day usage

---
+++ config.py
@@ -0,0 +1,2 @@
+dirname_input_files = "inputs"
+dirname_storage_files = "filestorage"
--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import json
+from os.path import join
+
+from config import dirname_storage_files
 
 
 def save_to_file(data: list[int], file_name: str):
-    with open(file_name, "w") as handle:
+    with open(join(dirname_storage_files, file_name), "w") as handle:
         handle.write(json.dumps(data))
 
 
 def read_from_file(file_name: str) -> list[int]:
-    with open(file_name, "r") as handle:
+    with open(join(dirname_storage_files, file_name), "r") as handle:
         return json.loads(handle.read())

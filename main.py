f51477664b5a961f17d29d125712b917dc98b079
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-12-06 22:20:39 +0100

    [feat]: added day 6

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     challenges.day_3.start_day_challenge()
     challenges.day_4.start_day_challenge()
     challenges.day_5.start_day_challenge()
-    # set "massive_loop" to True for challenge 6-2, but may take "some" time
+    # set "massive_loop" to True for challenge 6-2, but may take "some" time (reduced by like 80% of initial time)
     challenges.day_6.start_day_challenge(massive_loop=False)
